b5f14fa732382858737102cb921c9b3903a2e646
trainerdev <fatihcane0696@outlook.com>
2022-11-13 23:04:11 +0300

    location options 2

--- app/src/main/java/com/fcadev/kotlinmaps/MapsActivity.kt
+++ app/src/main/java/com/fcadev/kotlinmaps/MapsActivity.kt
@@ -1,6 +1,7 @@
 package com.fcadev.kotlinmaps
 
 import android.Manifest
+import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
@@ -29,6 +30,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var locationManager : LocationManager
     private lateinit var locationListener: LocationListener
     private lateinit var permissionLauncher : ActivityResultLauncher<String>
+    private lateinit var sharedPreferences: SharedPreferences
+    private var trackBoolean : Boolean? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,6 +45,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
 
         registerLauncher()
+
+        sharedPreferences = this.getSharedPreferences("com.fcadev.kotlinmaps", MODE_PRIVATE)
+        trackBoolean = false
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
@@ -52,7 +58,12 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         locationListener = object : LocationListener{
             override fun onLocationChanged(p0: Location) {
-                println("Location: " + p0.toString())
+                trackBoolean = sharedPreferences.getBoolean("trackBoolean", false)
+                if (trackBoolean == false){
+                    val userLocation = LatLng(p0.latitude, p0.longitude)
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 15f))
+                    sharedPreferences.edit().putBoolean("trackBoolean", true).apply()
+                }
             }
         }
 
@@ -69,6 +80,12 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         }else{
             //permission granted
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, locationListener)
+            val lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+            if (lastLocation != null){
+                val lastUserLocation = LatLng(lastLocation.latitude, lastLocation.longitude)
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lastUserLocation, 15f))
+            }
+            mMap.isMyLocationEnabled = true
         }
 
         //Hazır kordinat ile konum belirtmek.
@@ -85,6 +102,12 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED){
                     //permission granted
                     locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, locationListener)
+                    val lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+                    if (lastLocation != null){
+                        val lastUserLocation = LatLng(lastLocation.latitude, lastLocation.longitude)
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lastUserLocation, 15f))
+                    }
+                    mMap.isMyLocationEnabled = true
                 }
             }else{
                 //permission denied
