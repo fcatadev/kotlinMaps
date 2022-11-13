c0e4d244258d2839083be9a7bb4b28206071d670
trainerdev <fatihcane0696@outlook.com>
2022-11-13 21:29:36 +0300

    location options

--- app/src/main/java/com/fcadev/kotlinmaps/MapsActivity.kt
+++ app/src/main/java/com/fcadev/kotlinmaps/MapsActivity.kt
@@ -1,5 +1,8 @@
 package com.fcadev.kotlinmaps
 
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -15,6 +18,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+    private lateinit var locationManager : LocationManager
+    private lateinit var locationListener: LocationListener
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,11 +36,22 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
+        //casting
+        locationManager = this.getSystemService(LOCATION_SERVICE) as LocationManager
+
+        locationListener = object : LocationListener{
+            override fun onLocationChanged(p0: Location) {
+
+            }
+        }
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, locationListener)
+
+        //Hazır kordinat ile konum belirtmek.
         // latitude 48.8583736, longitude 2.2922926
 
-        // Add a marker in Sydney and move the camera
-        val eiffel = LatLng(48.8583736, 2.2922926)
-        mMap.addMarker(MarkerOptions().position(eiffel).title("Eiffel Tower"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(eiffel, 15f))
+        //val eiffel = LatLng(48.8583736, 2.2922926)
+        //mMap.addMarker(MarkerOptions().position(eiffel).title("Eiffel Tower"))
+        //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(eiffel, 15f))
     }
 }
